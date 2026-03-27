1b175f4d9215d9b794ad9bdc794d0290a035e967
cyber-gentle <info.abdavid@gmail.com>
2026-03-27 12:02:57 +0100

    update main.go

--- thecodecrafterthon-day1/cli.go
+++ thecodecrafterthon-day1/cli.go
@@ -30,20 +30,20 @@ start:
 
 		case1FirstNumber:
 			fmt.Print("Enter first number: ")
-			number1, err := fmt.Scanln(&num1)
+			_, err := fmt.Scanln(&num1)
 			if err != nil {
 				fmt.Println("Enter a number and not alphabet")
 				goto case1FirstNumber
 			}
 		case1SecondNumber:
 			fmt.Print("Enter second number: ")
-			number2, err := fmt.Scanln(&num2)
+			_, err = fmt.Scanln(&num2)
 			if err != nil {
 				fmt.Println("Enter a number and not alphabet")
 				goto case1SecondNumber
 			}
-			result := number1 + number2
-			fmt.Printf("%d + %d is = %d\n", number1, number2, result)
+			result := num1 + num2
+			fmt.Printf("%d + %d is = %d\n", num1, num2, result)
 			fmt.Println()
 
 			fmt.Print("Do you want add another set of numbers?\n 1. Yes, I want to.\n 2. No, Quit.\n 3. Go to Menu\n")
@@ -63,20 +63,20 @@ start:
 		case 2:
 		case2FirstNumber:
 			fmt.Print("Enter first number: ")
-			number1, err := fmt.Scanln(&num1)
+			_, err := fmt.Scanln(&num1)
 			if err != nil {
 				fmt.Println("Enter a number and not alphabet")
 				goto case2FirstNumber
 			}
 		case2SecondNumber:
 			fmt.Print("Enter second number: ")
-			number2, err := fmt.Scanln(&num2)
+			_, err = fmt.Scanln(&num2)
 			if err != nil {
 				fmt.Println("Enter a number and not alphabet")
 				goto case2SecondNumber
 			}
-			result := number1 - number2
-			fmt.Printf("%d - %d is = %d\n", number1, num2, result)
+			result := num1 - num2
+			fmt.Printf("%d - %d is = %d\n", num1, num2, result)
 
 			fmt.Print("Do you want subtract another set of numbers?\n 1. Yes, I want to.\n 2. No, Quit.\n 3. Go to Menu\n")
 			fmt.Print("Pick your choice: ")
@@ -95,20 +95,20 @@ start:
 		case 3:
 		case3FirstNumber:
 			fmt.Print("Enter first number: ")
-			number1, err := fmt.Scanln(&num1)
+			_, err := fmt.Scanln(&num1)
 			if err != nil {
 				fmt.Println("Enter a number and not alphabet")
 				goto case3FirstNumber
 			}
 		case3SecondNumber:
 			fmt.Print("Enter second number: ")
-			number2, err := fmt.Scanln(&num2)
+			_, err = fmt.Scanln(&num2)
 			if err != nil {
 				fmt.Println("Enter a number and not alphabet")
 				goto case3SecondNumber
 			}
-			result := number1 * number2
-			fmt.Printf("%d x %d is = %d\n", number1, number2, result)
+			result := num1 * num2
+			fmt.Printf("%d x %d is = %d\n", num1, num2, result)
 
 			fmt.Print("Do you want multiply another set of numbers?\n 1. Yes, I want to.\n 2. No, Quit.\n 3. Go to Menu\n")
 			fmt.Print("Pick your choice: ")
@@ -127,14 +127,14 @@ start:
 		case 4:
 		case4FirstNumber:
 			fmt.Print("Enter first number: ")
-			number1, err := fmt.Scanln(&num1)
+			_, err := fmt.Scanln(&num1)
 			if err != nil {
 				fmt.Println("Enter a number and not alphabet")
 				goto case4FirstNumber
 			}
 		case4SecondNumber:
 			fmt.Print("Enter second number: ")
-			number2, err := fmt.Scanln(&num1)
+			_, err = fmt.Scanln(&num2)
 			if err != nil {
 				fmt.Println("Enter a number and not alphabet")
 				goto case4SecondNumber
@@ -143,8 +143,8 @@ start:
 				fmt.Println("Divisor can't be zero")
 				goto case4SecondNumber
 			}
-			result := number1 / number2
-			fmt.Printf("%d ÷ %d is = %d\n", number1, number2, result)
+			result := num1 / num2
+			fmt.Printf("%d ÷ %d is = %d\n", num1, num2, result)
 
 			fmt.Print("Do you want divide another set of numbers?\n 1. Yes, I want to.\n 2. No, Quit.\n 3. Go to Menu\n")
 			fmt.Print("Pick your choice: ")
