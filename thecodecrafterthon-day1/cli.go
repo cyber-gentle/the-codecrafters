48a44d889d4286d808baa26969201ea7bf504101
cyber-gentle <info.abdavid@gmail.com>
2026-03-27 10:24:18 +0100

    update main.go

--- thecodecrafterthon-day1/cli.go
+++ thecodecrafterthon-day1/cli.go
@@ -10,19 +10,20 @@ import (
 
 var (
 	num1, num2 int // integer storer
-	p_choice int // proceed choice
+	p_choice   int // proceed choice
 )
 
 func main() {
 
 	reader := bufio.NewReader(os.Stdin)
-	fmt.Print("CHOOSE AN OPERATION TO EXECUTE\n 1. Addition\n 2. Subtraction\n 3. Multiplication\n 4. Division\n 5. Exit\n")
+	fmt.Printf("CHOOSE AN OPERATION TO EXECUTE\n 1. Addition\n 2. Subtraction\n 3. Multiplication\n 4. Division\n 5. Exit\n Type %q for more info\n", "HELP")
 	fmt.Print("Select an operation to continue: ")
 	op_c, _ := reader.ReadString('\n')
 	op_c = strings.TrimSpace(op_c)
 	op, _ := strconv.Atoi(op_c)
 
 	for {
+
 		switch op {
 		case 1:
 			fmt.Print("Enter first number: ")
@@ -31,8 +32,10 @@ func main() {
 			fmt.Scanln(&num2)
 			result := num1 + num2
 			fmt.Printf("%d + %d is = %d\n", num1, num2, result)
+			fmt.Println()
 
-			fmt.Println("Do you want add another set of numbers?\n 1. Yes\n 2.No\n")
+			fmt.Print("Do you want add another set of numbers?\n 1. Yes\n 2. No\n")
+			fmt.Print("Pick your choice: ")
 			fmt.Scanln(&p_choice)
 
 			if p_choice == 1 {
@@ -51,6 +54,18 @@ func main() {
 			result := num1 - num2
 			fmt.Printf("%d - %d is = %d\n", num1, num2, result)
 
+			fmt.Print("Do you want subtract another set of numbers?\n 1. Yes\n 2. No\n")
+			fmt.Print("Pick your choice: ")
+			fmt.Scanln(&p_choice)
+
+			if p_choice == 1 {
+				continue
+			} else if p_choice == 2 {
+				return
+			} else {
+				fmt.Println("Choose from the options above")
+			}
+
 		case 3:
 			fmt.Print("Enter first number: ")
 			fmt.Scanln(&num1)
@@ -59,16 +74,49 @@ func main() {
 			result := num1 * num2
 			fmt.Printf("%d x %d is = %d\n", num1, num2, result)
 
+			fmt.Print("Do you want multiply another set of numbers?\n 1. Yes\n 2. No\n")
+			fmt.Print("Pick your choice: ")
+			fmt.Scanln(&p_choice)
+
+			if p_choice == 1 {
+				continue
+			} else if p_choice == 2 {
+				return
+			} else {
+				fmt.Println("Choose from the options above")
+			}
+
 		case 4:
 			fmt.Print("Enter first number: ")
 			fmt.Scanln(&num1)
 			fmt.Print("Enter second number: ")
 			fmt.Scanln(&num2)
+			if num2 == 0 {
+				fmt.Println("Divisor can't be zero")
+				continue
+			}
 			result := num1 / num2
 			fmt.Printf("%d ÷ %d is = %d\n", num1, num2, result)
 
+			fmt.Print("Do you want divide another set of numbers?\n 1. Yes\n 2. No\n")
+			fmt.Print("Pick your choice: ")
+			fmt.Scanln(&p_choice)
+
+			if p_choice == 1 {
+				continue
+			} else if p_choice == 2 {
+				return
+			} else {
+				fmt.Println("Choose from the options above")
+			}
+
 		case 5:
 			fmt.Println("Exiting...")
+			return
+
+		}
+		if op_c == "HELP" {
+			fmt.Println(" Addition: add your input together\n Subtraction: Minus a number from another\n Multiplication: Multiply numbers to get result\n Division: Divides a number by the other\n ")
 			return
 		}
 	}
--- thecodecrafterthon-day1/go.mod
+++ thecodecrafterthon-day1/go.mod
@@ -1,3 +1,3 @@
-module UI-test2
+module thecodecrafterthon
 
 go 1.22.2
