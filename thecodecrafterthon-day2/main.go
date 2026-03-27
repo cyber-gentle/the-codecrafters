593252a12c2dfcbbb01ae71d3e7c93aa6b1fee92
cyber-gentle <info.abdavid@gmail.com>
2026-03-27 15:33:04 +0100

    update main.go

--- thecodecrafterthon-day2/main.go
+++ thecodecrafterthon-day2/main.go
@@ -5,6 +5,12 @@ import (
 	"strconv"
 )
 
+// func decimalToOthers(s string, base int) (float64, error) {
+// 	conv, _ := strconv.Atoi(s)
+// 	conv = float64(conv)
+// 	return strconv.FormatInt(conv, base, 64)
+// }
+
 func baseToDecimal(s string, base int) (int64, error) {
 	return strconv.ParseInt(s, base, 64)
 }
@@ -14,6 +20,10 @@ func binToDecimal(s string, base int) (int64, error) {
 }
 
 func main() {
-	fmt.Println(baseToDecimal("1E", 16))
-	fmt.Println(binToDecimal("1001", 2))
+	fmt.Println("       Welcome!")
+	fmt.Println(" Select Base to covert\n 1. Decimal to Binary and HexaDecimal\n 2. HexaDecimal to Decimal\n 3. Binary to Decimal")
+	fmt.Println("Pick any one to continue: ")
+
+	// fmt.Println(baseToDecimal("1E", 16))
+	// fmt.Println(binToDecimal("1001", 2))
 }
