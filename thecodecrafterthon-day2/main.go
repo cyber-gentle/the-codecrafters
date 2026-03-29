f442ea0705758778ec9a90a146f41e6fce342b82
Abraham David <info.abdavid@gmail.com>
2026-03-29 17:43:36 -0400

    Update main.go

--- thecodecrafterthon-day2/main.go
+++ thecodecrafterthon-day2/main.go
@@ -10,26 +10,22 @@ import (
 
 var ()
 
-// func decimalToOthers(s string, base int) (float64, error) {
-//	conv, _ := strconv.Atoi(s)
-//	conv = float64(conv)
-//	return strconv.FormatInt(conv, base, 64)
-//}
-
-func hexToDecimal(s string, base int) (int64, error) {
-	dec, err := strconv.ParseInt(s, base, 64)
-	if err != nil {
-		fmt.Println("Invalid Hexadecimal number.")
-	}
-	return dec, err
+func decimalToOthers(s string, base int) {
+	val, _ := strconv.ParseInt(s, base, 64)
+	hexaNumber := strconv.FormatInt(val, 16)
+	binNumber := strconv.FormatInt(val, 2)
+	fmt.Println(hexaNumber)
+	fmt.Printf("The converted Decimal is \n Binary: %v \n HexaDecimal: %X ", binNumber, hexaNumber)
 }
 
-func binToDecimal(n string, base int) (int64, error) {
-	bin, err := strconv.ParseInt(n, base, 64)
-	if err != nil {
-		fmt.Println("Invalid Binary number.")
-	}
-	return bin, err
+func hexToDecimal(s string, base int) int64 {
+	decimal, _ := strconv.ParseInt(s, base, 64)
+	return decimal
+}
+
+func binToDecimal(s string, base int) int64 {
+	decimal, _ := strconv.ParseInt(s, base, 64)
+	return decimal
 }
 
 func main() {
@@ -46,27 +42,49 @@ start:
 		switch c_choice {
 		case 1:
 		case1Start:
-			fmt.Print("Enter a Decimal Nmuber to convert to Binary and HexaDecimal: ")
+			fmt.Print("Enter a valid Decimal Number to convert to HexaDecimal and Binary: ")
 			number, _ := reader.ReadString('\n')
 			number = strings.TrimSpace(number)
+
 			if number == "" {
-				fmt.Println("It's an Empty string, Enter a valid HexaDecimal number")
+				fmt.Println()
+				fmt.Print("It's an Empty string, Enter a valid Decimal number\n \n")
 				goto case1Start
 			}
-			return
+
+			decimalToOthers("20", 10)
+			//fmt.Printf("The result of Decimal converted to other bases are \n  Binary: %q \n HexaDecimal: %d\n \n", decimalToOthers("20", 10))
+
+			fmt.Print("Do you have another Decimal number to convert?\n 1. Yes\n 2. No, Quit\n 3. Go to Menu\n Select choice here: ")
+			choice, _ := reader.ReadString('\n')
+			choice = strings.TrimSpace(choice)
+			choice_number, _ := strconv.Atoi(choice) // cleaned choice after atoi
+
+			if choice_number == 1 {
+				fmt.Println()
+				continue
+			} else if choice_number == 2 {
+				return
+			} else if choice_number == 3 {
+				fmt.Println()
+				goto start
+			}
+
 		case 2:
 		case2Start:
 			fmt.Println()
 			fmt.Print("Enter a valid HexaDecimal Number to convert to Decimal: ")
 			number, _ := reader.ReadString('\n')
 			number = strings.TrimSpace(number)
+
 			if number == "" {
-				fmt.Println("It's an Empty string, Enter a valid HexaDecimal number")
+				fmt.Println()
+				fmt.Print("It's an Empty string, Enter a valid HexaDecimal number\n \n")
 				goto case2Start
 			}
 
 			base := 16
-			fmt.Println(hexToDecimal(number, base))
+			fmt.Printf("The Decimal result of %q base %d is %d\n", number, base, hexToDecimal(number, base))
 
 			fmt.Println()
 			fmt.Print("Do you have another HexaDecimal number to convert?\n 1. Yes\n 2. No, Quit\n 3. Go to Menu\n Select choice here: ")
@@ -75,38 +93,49 @@ start:
 			choice_number, _ := strconv.Atoi(choice) // cleaned choice after atoi
 
 			if choice_number == 1 {
+				fmt.Println()
 				continue
 			} else if choice_number == 2 {
 				return
 			} else if choice_number == 3 {
+				fmt.Println()
 				goto start
 			}
 
 		case 3:
-			//case3Start:
+		case3Start:
 			fmt.Println()
 			fmt.Print("Enter a valid Binary Number to convert to Decimal: ")
 			number, _ := reader.ReadString('\n')
 			number = strings.TrimSpace(number)
 
-			base := 2
-			fmt.Println(binToDecimal(number, base))
-			//goto case3Start
+			if number == "" {
+				fmt.Print("It's an Empty string, Enter a valid HexaDecimal number\n \n")
+				goto case3Start
+			}
 
+			base := 2
+			fmt.Printf("The Decimal result of %q base %d is %d\n", number, base, binToDecimal(number, base))
+
+		choose_choice:
 			fmt.Println()
 			fmt.Print("Do you have another Binary number to convert\n 1. Yes\n 2. No, Quit\n 3. Go to Menu\n Select choice here: ")
 			choice, _ := reader.ReadString('\n')
 			choice = strings.TrimSpace(choice)
 			choice_number, _ := strconv.Atoi(choice) // cleaned choice after atoi
 			if choice == "" {
-				fmt.Println("It's an Empty string, Enter a valid Binary number")
+				fmt.Print("It's an Empty string, Choose from the options above\n \n")
+				goto choose_choice
+
 			}
 
 			if choice_number == 1 {
+				fmt.Println()
 				continue
 			} else if choice_number == 2 {
 				return
 			} else if choice_number == 3 {
+				fmt.Println()
 				goto start
 			}
 
