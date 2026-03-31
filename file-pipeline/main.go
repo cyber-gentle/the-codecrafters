601a30827208afc1fe695f243c5000a19379342e
cyber-gentle <info.abdavid@gmail.com>
2026-03-31 21:58:12 +0100

    program startup

--- file-pipeline/main.go
+++ file-pipeline/main.go
@@ -1,4 +1,32 @@
-       // CodeCrafters — Operation Gopher Protocol
-       // Module: File Pipeline
-       // Author: Abraham David
-       // Squad:  [Your Squad Name]
+// CodeCrafters — Operation Gopher Protocol
+// Module: File Pipeline
+// Author: Abraham David
+// Squad:  [Your Squad Name]
+
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	file, err := os.Open("input.txt")
+	if err != nil {
+		fmt.Print(" Failed to open file\n No file name matches yours\n \n")
+	}
+
+	defer file.Close()
+
+	// input_content, err := os.ReadFile("file")
+	// if err != nil {
+	// 	fmt.Print(" Unable to read file\n \n")
+	// }
+
+	data := make([]byte, 200)
+	count, err := file.Read(data)
+	 if err != nil {
+              fmt.Println("Error in reading file")
+	}
+	fmt.Printf("%q\n", data[:count])
+}
