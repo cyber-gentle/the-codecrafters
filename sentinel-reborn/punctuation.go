126ce8d21041420b6561125b89a5f5bde5baf495
cyber-gentle <info.abdavid@gmail.com>
2026-04-01 21:00:50 +0100

    func

--- sentinel-reborn/punctuation.go
+++ sentinel-reborn/punctuation.go
@@ -4,21 +4,7 @@ import (
 	"strings"
 )
 
-func fixArticle(word string) string {
- words := strings.Fields(word)
- vowels := "aeiouhAEIOUH"
- for i := 0; i < len(words)-1; i++ {
-     if (words[i] == "a" || words[i] == "A") && strings.ContainsRune(vowels, rune(words[i+1][0])) {
-         if words[i] == "A" {
-             words[i] = "An"
-         } else {
-             words[i] = "an"
-         }
-     }
- }
- return strings.Join(words, " ")
-}
-
+// DONE
 func multiPunctuation(text string) string {
 	replacements := map[string]string{
 		" ...": "...",
@@ -35,6 +21,7 @@ func multiPunctuation(text string) string {
 	return text
 }
 
+// DONE
 func fixPunctuation(text string) string {
 	text = multiPunctuation(text)
 
@@ -49,4 +36,20 @@ func fixPunctuation(text string) string {
 	text = strings.ReplaceAll(text, " '", "'")
 
 	return strings.Join(strings.Fields(text), " ")
+}
+
+// DONE
+func fixArticle(word string) string {
+ words := strings.Fields(word)
+ vowels := "aeiouhAEIOUH"
+ for i := 0; i < len(words)-1; i++ {
+     if (words[i] == "a" || words[i] == "A") && strings.ContainsRune(vowels, rune(words[i+1][0])) {
+         if words[i] == "A" {
+             words[i] = "An"
+         } else {
+             words[i] = "an"
+         }
+     }
+ }
+ return strings.Join(words, " ")
 }
