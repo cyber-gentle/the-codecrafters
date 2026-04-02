e025fb30ddd5afff05915584ee76a65dd7c3779c
abrdavid <info.abdavid@gmail.com>
2026-04-02 14:31:59 -0400

    feat

--- string-transformer/main.go
+++ string-transformer/main.go
@@ -1,7 +1,7 @@
 // CodeCrafters — Operation Gopher Protocol
 // Module: String Transformer
 // Author: Abraham David
-// Squad:  Alpha Tiger
+// Squad:  The Gopher's
 
 package main
 
@@ -16,9 +16,17 @@ func toUpperCase(s string) string {
 	return strings.ToUpper(s)
 }
 
+func  toLowerCase(s string) string {
+	return strings.ToLower(s)
+}
+
+// func capitalizeCase(s string) string {
+// 	return strings.ToLower(s)
+// }
+
 func main() {
 
-	start:
+start:
 	reader := bufio.NewReader(os.Stdin)
 
 	fmt.Print("\n SENTINEL STRING TRANSFORMER — ONLINE\n ──────────────────────────────────────\n \n")
@@ -32,13 +40,18 @@ func main() {
 	word := split[1:]                // Get word from  user input in form of slice of strings
 	words := strings.Join(word, " ") // Joining word to back from slice to string
 
-		for {
-			switch cmd {
-			case "upper":
-				fmt.Printf(" → %v\n \n", toUpperCase(words))
-				goto start
+	fmt.Println(input_word, split, cmd, word, words)
 
-			}
+	for {
+		switch cmd {
+		case "upper":
+			fmt.Printf(" → %v\n \n", toUpperCase(words))
+			goto start
+
+		case "lower":
+			fmt.Printf(" → %v\n \n",  toLowerCase(words))
+			goto start
 		}
+	}
 
 }
